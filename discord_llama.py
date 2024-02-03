2d9204ce63efcdcffa9e4da48ef7b8bbc956bf37
Pat Wendorf <dungeons@gmail.com>
2024-02-02 20:11:23 -0500

    Prevent over 2000 character output to discord

--- discord_llama.py
+++ discord_llama.py
@@ -105,7 +105,8 @@ async def on_message(message):
     if client.user.mentioned_in(message):
         prompt = format_prompt(bot["question_prompt"], message.author.name, remove_id(message.content), history_text)
         direct_msg = True
-        await message.channel.send(llm_response(prompt))
+        bot_response = llm_response(prompt)
+        await message.channel.send(bot_response[:2000])
     
     # Figure out if someone said something we should respond to, besides @message these are configured in the identity.json
     comment_on_it = False
@@ -117,7 +118,8 @@ async def on_message(message):
     # But they should not respond if it was a direct message with the triggers in it
     if comment_on_it and random.random() <= float(bot["trigger_level"]) and direct_msg == False:
         prompt = format_prompt(bot["trigger_prompt"], message.author.name, remove_id(message.content), history_text)
-        await message.channel.send(llm_response(prompt))
+        bot_response = llm_response(prompt)
+        await message.channel.send(bot_response[:2000])
 
 # Run the main loop
 client.run(bot["discord_token"])
